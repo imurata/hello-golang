1594aa7572715230f843b5bffffd45e66b36d3d8
Toshiaki Maki <tmaki@pivotal.io>
2020-09-03 10:48:48 +0900

    Update main.go

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Hello Kpack!")
+		fmt.Fprint(w, "Hello TBS!")
 	})
 	http.ListenAndServe(":8080", nil)
 }
